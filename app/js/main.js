5628cae9b9bd2c272b374389408084cca9e2baab
ilya199006 <soulrebel0@gmail.com>
2019-03-17 20:50:48 +0300

    added main.js

---
+++ app/js/main.js
@@ -0,0 +1,8 @@
+window.onload = function () {
+  var count = document.getElementById('count');
+  var indexBody = document.getElementById('indexBody');
+  window.onscroll = function() {
+  	var scrolled = window.pageYOffset;
+    //count.innerHTML = scrolled;
+  }
+}
